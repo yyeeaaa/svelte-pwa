566ee2b81c272bf4c43ff4163d6ff802066eb1f9
yyeeaaa <145449580+yyeeaaa@users.noreply.github.com>
2023-09-26 09:21:12 +0000

    pwa

---
+++ public/sw.js
@@ -0,0 +1,36 @@
+const urlsToCache = [
+    "./**",
+    "./offline.html",
+    "./manifest.json",
+]
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open("static").then(cache => {
+                return cache.addAll(urlsToCache)
+            }
+        )
+    )
+})
+
+self.addEventListener("message", e => {
+    console.log(`The client sent me a message: ${e.data}`);
+
+    if(e.data == "update") {
+        console.log("Update")
+        e.waitUntil(
+            caches.open("static").then(cache => {
+                    return cache.addAll(urlsToCache)
+                }
+            )
+        )
+    }
+})
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    )
+})
--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,12 @@ const app = new App({
   target: document.getElementById('app'),
 })
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").then((reg) => {
+      console.log("Service worker registered.", reg);
+    });
+  });
+}
+
 export default app
